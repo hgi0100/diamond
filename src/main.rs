9f257ebf2f43883ef2202ee1d2d7c54ad671840b
Neil Crago <n.j.crago@gmail.com>
2024-07-05 08:48:16 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ use std::io::Write;
 mod scurve;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    
     // Write data to the output file, "diamond.csv"
     let out_file = OpenOptions::new()
         .read(true)
@@ -21,18 +22,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .create(true)
         .truncate(true)
         .open("diamond.csv");
+    
     let errheader = writeln!(
         out_file.as_ref().expect("REASON"),
         "time,S-Curve,Front Loaded,Back Loaded"
     );
+    
     if errheader.is_err() {
         panic!("Error writing to file: {errheader:?}");
     }
 
-    // logistic function s-curve
+    // Call s-curve function with 100 segments
     let pv2d = scurve::scurves::do_s_curve(&100);
 
     let mut i = 0;
+    
     for oo in pv2d {
         i += 1;
         let errval = writeln!(
